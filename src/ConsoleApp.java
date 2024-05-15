148e66750373f3df122a8a3d846c705641916280
Denis Merioara <merioara.denis@yandex.ru>
2024-05-15 08:55:31 +0300

    добавлена поддержка аргументов width и height

--- src/ConsoleApp.java
+++ src/ConsoleApp.java
@@ -1,9 +1,25 @@
 public class ConsoleApp {
+
     public static void main(String[] args) {
         String symbol = "*";
 
+        // проставляем значения по умолчанию
         int width = 10;
         int height = 5;
+
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i += 2) {
+                // в чётных элементах массива находятся названия аргументов, в нечётных — значения
+                String key = args[i];
+                String value = args[i + 1];
+
+                if (key.equals("--width")) {
+                    width = Integer.parseInt(value);
+                } else if (key.equals("--height")) {
+                    height = Integer.parseInt(value);
+                }
+            }
+        }
 
         for (int i = 1; i <= height; i++) {
             for (int j = 1; j <= width; j++) {
