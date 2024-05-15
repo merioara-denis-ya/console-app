062caba5f16c912a0ed8290f261cc08cfe8705e8
Denis Merioara <merioara.denis@yandex.ru>
2024-05-15 09:03:34 +0300

    Добавил поддержку аргумента border

--- src/ConsoleApp.java
+++ src/ConsoleApp.java
@@ -6,26 +6,34 @@ public class ConsoleApp {
         // проставляем значения по умолчанию
         int width = 10;
         int height = 5;
+        boolean needBorder = false;
+        String borderSymbol = "S";
 
         if (args.length > 0) {
-            for (int i = 0; i < args.length; i += 2) {
-                // в чётных элементах массива находятся названия аргументов, в нечётных — значения
-                String key = args[i];
-                String value = args[i + 1];
+            for (int i = 0; i < args.length; i += 1) {
+                String token = args[i];
 
-                if (key.equals("--width")) {
-                    width = Integer.parseInt(value);
-                } else if (key.equals("--height")) {
-                    height = Integer.parseInt(value);
-                } else if (key.equals("--symbol")) {
-                    symbol = value;
+                if (token.equals("--width")) {
+                    width = Integer.parseInt(args[i + 1]);
+                } else if (token.equals("--height")) {
+                    height = Integer.parseInt(args[i + 1]);
+                } else if (token.equals("--symbol")) {
+                    symbol = args[i + 1];
+                } else if (token.equals("--border")) {
+                    needBorder = true;
                 }
             }
         }
 
         for (int i = 1; i <= height; i++) {
             for (int j = 1; j <= width; j++) {
-                System.out.print(symbol);
+                // если нужна рамка, первый и последний символы в каждой строке и столбце становятся рамкой
+                if ( needBorder
+                        && ( i == 1 || j == 1 || i == height || j == width)) {
+                    System.out.print(borderSymbol);
+                } else {
+                    System.out.print(symbol);
+                }
             }
             System.out.println();
         }
